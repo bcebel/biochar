edb66651cc9abad1e35811df097ff324868191ff
bcebel <bcebel@gmail.com>
2023-09-14 22:03:21 -0400

    chatgpt

--- src/components/chatgpt.js
+++ src/components/chatgpt.js
@@ -1,23 +1,20 @@
 
 import React from "react";
+import chatgptphoto from "../ai.jpeg";
 
 export default function chatgpt() {
   // All functional components must have a return method that contains JSX.
   // We return all the JSX inside a parent element with a className of "container".
   return (
     <div className="container">
-      <h1>Renewable Carbon Negative Energy</h1>
-      <p>Take carbon out of the air for a hundred years or more.</p>
+      <h1>What does chat GPT know about biochar?</h1>
+      <img src={chatgptphoto} className="photos" alt="burning biochar" />
+      <p>It knows a thing or two! </p>
       <ul className="parent">
+        <li>see the chat I had with it.</li>
         <a href="https://chat.openai.com/share/547c9009-6ad8-4f65-9ddf-c65b4610c7c4">
           https://chat.openai.com/share/547c9009-6ad8-4f65-9ddf-c65b4610c7c4
         </a>
-        <li>Your source material can be from waste/scrap material</li>
-        <li>
-          Or you could grow a crop like hemp for biomass to convert to pure
-          hydrogen, oxygen and carbon
-        </li>
-        <li>Dirty hippies</li>
       </ul>
     </div>
   );
