8f02806990c68321a63c24dda73e1c368d49e88a
bcebel <bcebel@gmail.com>
2023-09-16 19:38:53 -0400

    background image

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Chatgpt from './components/chatgpt';
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/components/chatgpt.js
+++ src/components/chatgpt.js
@@ -7,13 +7,13 @@ export default function chatgpt() {
   // We return all the JSX inside a parent element with a className of "container".
   return (
     <div className="container">
-      <h1>What does chat GPT know about biochar?</h1>
+      <h1>What does AI know about biochar?</h1>
       <img src={chatgptphoto} className="photos" alt="burning biochar" />
       <p>It knows a thing or two! </p>
       <ul className="parent">
-        <li>see the chat I had with it.</li>
+ 
         <a href="https://chat.openai.com/share/547c9009-6ad8-4f65-9ddf-c65b4610c7c4">
-          https://chat.openai.com/share/547c9009-6ad8-4f65-9ddf-c65b4610c7c4
+          Click here to see the chat I had with chat-gpt.
         </a>
       </ul>
     </div>
